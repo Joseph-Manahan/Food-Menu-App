ebb8b495236a4098e2ff8d06bbb7e588d06b6b12
Joseph Manahan <jmanahan@mail.sfsu.edu>
2023-03-09 13:52:45 -0800

    Initial Commit

---
+++ foodTwoFuncs-Manahan/foodTwoFuncs_ManahanApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodTwoFuncs_ManahanApp.swift
+//  foodTwoFuncs-Manahan
+//
+//  Created by Joseph Manahan on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct foodTwoFuncs_ManahanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
